a1d56a4908ef31313540ad971d3ace84519e61a0
Robosword <32asingh@stratfordschools.net>
2026-05-04 19:41:55 +0000

    Find which age group and file we will be using for the user at least.

--- main.py
+++ main.py
@@ -5,3 +5,13 @@ import torch.nn.functional as F
 from sklearn.model_selection import train_test_split
 
 age = int(input("What is your age? "))
+ageGroup = ""
+if 5 <= age <= 8:
+    ageGroup = "58.csv"
+elif 9 <= age <= 13:
+    ageGroup = "913.csv"
+elif 14 <= x <= 17:
+    ageGroup = "1417.csv"
+else:
+    ageGroup = "MA.csv" 
+   
