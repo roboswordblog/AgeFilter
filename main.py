1470c9e7c3419d05b14628a88e58380c2b7223ba
Robosword <32asingh@stratfordschools.net>
2026-05-04 19:45:13 +0000

    importing the tfidf thing from sklearn

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 age = int(input("What is your age? "))
 ageGroup = ""
