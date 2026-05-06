2fba2b3e3e3062244ccfdaf0486384cc1d68fd74
Robosword <32asingh@stratfordschools.net>
2026-05-06 06:13:53 +0000

    make it write the files the new user

--- dataManage.py
+++ dataManage.py
@@ -1,12 +1,13 @@
 def getAllUsers():
-    files = open("webData/users.csv")
+    files = open("webData/users.csv", "r")
     users = []
     for line in files.read().split("\n"):
         users.append(line.split(",")[0])
     return users
 
-def addUsers():
-    pass
+def addUsers(username,password,age):
+    files = open("webData/users.csv", "a")
+    files.write(f"{username},{password},{age}")
 
 def checkUsers():
     pass
