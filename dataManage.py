7b04fcbe32f662c0958efb5bbeb972447ad436b3
Robosword <32asingh@stratfordschools.net>
2026-05-06 21:17:20 +0000

    add some stuff to make it easier to work and stuff

--- dataManage.py
+++ dataManage.py
@@ -8,12 +8,12 @@ def getAllUsers():
 
 def addUsers(username,password,age):
     files = open("webData/users.csv", "a")
-    files.write(f"{username},{password},{age}\n")
+    files.write(f"\n{username},{password},{age}")
     files.close()
 
 def addMessage(user, message):
     files = open("webData/chatLogs.csv", "a")
-    files.write(f"{user},{message}\n")
+    files.write(f"\n{user},{message}")
     files.close()
 
 
