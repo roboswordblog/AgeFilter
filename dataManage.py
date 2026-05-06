4a61221629151f1d3d954b94c64c28dc3f819e2f
Robosword <32asingh@stratfordschools.net>
2026-05-06 05:48:39 +0000

    create the get all users function and make it return all the users

--- dataManage.py
+++ dataManage.py
@@ -1,5 +1,9 @@
 def getAllUsers():
-    pass
+    files = open("webData/users.csv")
+    users = []
+    for line in files.read().split("\n"):
+        users.append(line.split(",")[0])
+    return users
 
 def addUsers():
     pass
