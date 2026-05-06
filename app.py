ceb4a9b95988ccab6b84644234b7897146a46c69
Robosword <32asingh@stratfordschools.net>
2026-05-06 05:52:26 +0000

    Make the sendMessage and getMessage functions

--- app.py
+++ app.py
@@ -29,4 +29,15 @@ def signup():
 def signupData():
     return {"takenUsers": getAllUsers()}
 
+@app.route("/sendMessage")
+def sendMessage():
+    # add the message to the rest of the messages in the file along with the username
+    pass
+
+@app.route("/getAllMessages")
+def getMessages():
+    # get all the messages from the file and see which ones the frontend doesn't have
+    pass
+
+
 app.run(debug=True)
