4529b401b10f4b640301300ec45db29fdb428fff
Robosword <32asingh@stratfordschools.net>
2026-05-07 06:27:49 +0000

    okWModel

--- main.py
+++ main.py
@@ -88,7 +88,7 @@ with torch.no_grad():
     model.eval()
 appropriateMap = {
     0:"Approprate",
-    1:"NONE APPROPRiATE"
+    1:"NONAPPROPRiATE"
 }
 user_input = input("\nEnter a message (or type 'quit' to stop): ")
 input_vector = vectorizer.transform([user_input]).toarray()
